377f149ad5eaf649f26123bf839c554381c4e2ca
yinamsu <yinamsu@gmail.com>
2024-08-31 09:06:01 +0900

    31st Aug 2024

--- jinstagram/urls.py
+++ jinstagram/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from .views import Main, UploadFeed
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', Main.as_view()),
+    path('content/upload', UploadFeed.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
